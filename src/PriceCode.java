c969fe9e3c2a2d326f6c2c5cb9d1ee6533f758f0
Lukas Rapp <lu.rapp@web.de>
2022-05-10 12:07:20 +0200

    Simplified PriceCode Class

--- src/PriceCode.java
+++ src/PriceCode.java
@@ -1,15 +1,5 @@
 public enum PriceCode {
-    REGULAR(0),
-    NEW_RELEASE(1),
-    CHILDREN(2);
-    
-    PriceCode(int id) {
-        this.id = id;
-    }
-    
-    private int id;
-    
-    public int getID() {
-        return this.id;
-    }
+    REGULAR(),
+    NEW_RELEASE(),
+    CHILDREN();
 }
