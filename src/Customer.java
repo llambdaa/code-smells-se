560fb394ed745804438f8e15abfb1a5a259f1242
Lukas Rapp <lu.rapp@web.de>
2022-05-10 11:48:35 +0200

    Extracted Method as Member Function of Object Called Upon
    It makes much more sense to provide the calculation of
    the rental cost by making a call upon the target object
    that can be used anywhere instead of making a function
    with similar functionality in an unrelated other class.

--- src/Customer.java
+++ src/Customer.java
@@ -25,7 +25,7 @@ class Customer {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getRentalPrice();
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -39,24 +39,6 @@ class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
-    }
-
-    private double amountFor(Rental each) {
-        int rented = each.getDaysRented();
-        switch (each.getMovie().getPriceCode()) {
-            case REGULAR: {
-                return 2 + (rented > 2 ? (rented - 2) * 1.5 : 0);
-            }
-            case NEW_RELEASE: {
-                return rented * 3;
-            }
-            case CHILDREN: {
-                return 1.5 + (rented > 3 ? (rented - 3) * 1.5 : 0);
-            }
-            default: {
-                return 0;
-            }
-        }
     }
 
 }
--- src/Rental.java
+++ src/Rental.java
@@ -1,14 +1,32 @@
 class Rental {
     private Movie movie;
     private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
     public int getDaysRented() {
         return daysRented;
     }
+    
     public Movie getMovie() {
         return movie;
+    }
+    
+    public double getRentalPrice() {
+        switch (movie.getPriceCode()) {
+            case REGULAR: {
+                return 2 + (daysRented > 2 ? (daysRented - 2) * 1.5 : 0);
+            }
+            case NEW_RELEASE: {
+                return daysRented * 3;
+            }
+            case CHILDREN: {
+                return 1.5 + (daysRented > 3 ? (daysRented - 3) * 1.5 : 0);
+            }
+            default: {
+                return 0;
+            }
+        }
     }
 }
