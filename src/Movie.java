7b40eadbf9cc827fa2220baa85ca5c3f49ed362a
Lukas Rapp <lu.rapp@web.de>
2022-05-10 11:29:19 +0200

    Replace Constants Range with Enumeration

--- src/Movie.java
+++ src/Movie.java
@@ -1,20 +1,19 @@
 public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
-    public Movie(String newtitle, int newpriceCode) {
+    private PriceCode code;
+    public Movie(String newtitle, PriceCode code) {
         title = newtitle;
-        priceCode = newpriceCode;
+        this.code = code;
     }
-    public int getPriceCode() {
-        return priceCode;
+    public PriceCode getPriceCode() {
+        return this.code;
     }
-    public void setPriceCode(int arg) {
-        priceCode = arg;
+    
+    public void setPriceCode(PriceCode code) {
+        this.code = code;
     }
-    public String getTitle (){
+    
+    public String getTitle () {
         return title;
-    };
+    }
 }
