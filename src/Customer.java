53ebea2db43ced94cbb5741f5c4c467a30890ab0
Lukas Rapp <lu.rapp@web.de>
2022-05-10 12:09:56 +0200

    Replaced Type Erasured Container With Polymorph List

--- src/Customer.java
+++ src/Customer.java
@@ -4,35 +4,38 @@ import java.util.*;
 
 class Customer {
     private String name;
-    private Vector rentals = new Vector();
-    public Customer (String newname){
-        name = newname;
-    };
-    public void addRental(Rental arg) {
-        rentals.addElement(arg);
-    };
+    private List<Rental> rentals;
+    
+    public Customer (String name){
+        this.name = name;
+        this.rentals = new ArrayList<>();
+    }
+    
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+    
     public String getName (){
         return name;
-    };
+    }
+    
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
+        for (Rental rental : rentals) {
             double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            thisAmount = each.getRentalPrice();
+            thisAmount = rental.getRentalPrice();
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == PriceCode.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((rental.getMovie().getPriceCode() == PriceCode.NEW_RELEASE) && rental.getDaysRented() > 1)
                 frequentRenterPoints ++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
